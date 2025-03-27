df054ad75365cc0b0f83eb1dd29f8cbc8703021f
talissant <124712445+talissant@users.noreply.github.com>
2025-03-26 22:15:21 -0300

    alterações

--- gasolina/src/AppCal.jsx
+++ gasolina/src/AppCal.jsx
@@ -6,23 +6,18 @@ export default function AppCal() {
   const [gasolinaPreco, setGasolinaPreco] = useState("");
   const [resultado, setResultado] = useState("");
 
-  const calcularMelhorOpcao = () => {
-    const alcool = parseFloat(alcoolPreco.replace(",", "."));
-    const gasolina = parseFloat(gasolinaPreco.replace(",", "."));
-
-    if (isNaN(alcool) || isNaN(gasolina)) {
-      setResultado("Por favor, insira um valor válido.");
-      return;
-    }
-
-    const proporcao = alcool / gasolina;
-
-    if (proporcao < 0.7) {
-      setResultado("Álcool é a melhor opção.");
+  function calcularMelhorOpcao() {
+    if (alcoolPreco && gasolinaPreco) {
+      if (alcoolPreco / gasolinaPreco < 0.7) {
+        setResultado("Álcool é a melhor opção.");
+      } else {
+        setResultado("Gasolina é a melhor opção.");
+      }
     } else {
-      setResultado("Gasolina é a melhor opção.");
+      setResultado("Por favor, insira valores válidos.");
     }
-  };
+  }
+
   return (
     <>
       <AppTask
--- gasolina/src/AppTask.jsx
+++ gasolina/src/AppTask.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import logo from "./assets/logo.png";
 import "./App.css";
 
 export default function AppTask({
@@ -11,21 +12,27 @@ export default function AppTask({
   return (
     <div className="container">
       <h1>Qual melhor opção?</h1>
+      <img className="logo" src={logo} alt="bomba de combustivel" />
 
-      <label>Preço do Álcool (R$):</label>
-      <input
-        type="text"
-        value={alcoolPreco}
-        onChange={(e) => onAlcoolChange(e.target.value)}
-      />
+      <main>
+        <label>Preço do Álcool (R$):</label>
+        <input
+          type="number"
+          name=""
+          id=""
+          value={alcoolPreco}
+          onChange={(e) => onAlcoolChange(e.target.value)}
+        />
 
-      <label>Preço da Gasolina (R$):</label>
-      <input
-        type="text"
-        value={gasolinaPreco}
-        onChange={(e) => onGasolinaChange(e.target.value)}
-      />
-
+        <label>Preço da Gasolina (R$):</label>
+        <input
+          type="number"
+          name=""
+          id=""
+          value={gasolinaPreco}
+          onChange={(e) => onGasolinaChange(e.target.value)}
+        />
+      </main>
       <button onClick={onCalcular}>Calcular</button>
     </div>
   );
